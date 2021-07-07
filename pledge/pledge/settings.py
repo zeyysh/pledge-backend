991a0b39fbe374940d9c27b188d2c28c71b53008
Zeyysh <zeynab1717@gmail.com>
2021-07-07 12:06:20 +0430

    Second Commit

--- pledge/pledge/settings.py
+++ pledge/pledge/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
-
+ALLOWED_HOSTS = ['127.0.0.1', '193.176.243.57']
 
 # Application definition
 

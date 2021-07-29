57796e72d4ea103aef810cabb8b95c79a4f58372
Zeyysh <zeynab1717@gmail.com>
2021-07-29 17:13:03 +0430

    csrf and google anf facebook fixed

--- app/pledge/urls.py
+++ app/pledge/urls.py
@@ -7,22 +7,20 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-from users.views import empty_view, FacebookLogin
+from users.views import empty_view, FacebookLogin, GoogleLogin
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Pledge API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="zeynab1717@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
-# schema_view = get_schema_view(title='API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
-
 
 urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -33,12 +31,13 @@ urlpatterns = [
     path('api/register/verify-email/', empty_view, name='account_email_verification_sent'),
     path('password-reset/<uidb64>/<token>/', empty_view, name='password_reset_confirm'),
     url('api/facebook/$', FacebookLogin.as_view(), name='fb_login'),
+    url('api/google/$', GoogleLogin.as_view(), name='google_login'),
     path('users/', include('users.urls')),
     path("admin/", admin.site.urls),
-    path('document/', include('document.urls')),
-    path('notification/', include('notification.urls')),
-    path('payment/', include('payment.urls')),
-    path('pledge_app/', include('pledge_app.urls')),
+    # path('document/', include('document.urls')),
+    # path('notification/', include('notification.urls')),
+    # path('payment/', include('payment.urls')),
+    # path('pledge_app/', include('pledge_app.urls')),
 ]
 
 if bool(settings.DEBUG):
--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,6 @@
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.http import HttpResponse
 from rest_auth.registration.views import SocialLoginView
 from rest_framework import generics
@@ -11,6 +13,15 @@ from rest_framework.viewsets import ViewSet
 
 from users.models import User
 from users.serializers import UserSerializer
+
+
+class CustomGoogleOAuth2Adapter(GoogleOAuth2Adapter):
+    basic_auth = False
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = CustomGoogleOAuth2Adapter
+    client_class = OAuth2Client
 
 
 class FacebookLogin(SocialLoginView):

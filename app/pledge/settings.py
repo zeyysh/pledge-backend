92dd9a8bcd9723dad7bbcedd80ff9d59e3d4c758
Zeyysh <zeynab1717@gmail.com>
2021-08-01 19:11:22 +0430

    logout fix

--- app/pledge/settings.py
+++ app/pledge/settings.py
@@ -118,8 +118,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
